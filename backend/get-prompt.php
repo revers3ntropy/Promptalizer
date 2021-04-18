4f3257416362ff1be296c07694a8d6d86eb9235f
revers3ntropy <josephcoppin@gmail.com>
2021-04-18 18:05:40 +0100

    fetch JSON from the backend and show it to the user

--- backend/get-prompt.php
+++ backend/get-prompt.php
@@ -1,2 +1,4 @@
 <?php
-require('query-sql.php');
+//require('query-sql.php');
+
+echo "{\"prompt\": \"hi\"}";
--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function generate () {
+const state = {
+    type: 'poem'
+}
 
+function generate () {
+    fetch('./backend/get-prompt.php', {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(state) // body data type must match "Content-Type" header
+    })
+        .then(data => data.json())
+        .then(data => {
+            $('#content').html(data['prompt']);
+        })
+        .then(() => console.log('Got data!'))
 }
