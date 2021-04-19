c2fbd3bbeb8c1c033cba2ff2307cf4568bfe8743
revers3ntropy <josephcoppin@gmail.com>
2021-04-19 18:12:45 +0100

    redone in React with Typescript

--- backend/get-prompt.php
+++ backend/get-prompt.php
@@ -1,36 +1,30 @@
 <?php
 
 function getPoemPrompt($args) {
-	$data = [];
-	$data["type"] = "poem";
-	$data["lines"] = 6;
-	$data["rhyme"] = "ababab";
-	$data["keyword"] = "fish";
-	return json_encode($data);
+    $data = [];
+    $data["type"] = "poem";
+    $data["lines"] = 6;
+    $data["rhyme"] = "ababab";
+    $data["keyword"] = "fish";
+    return json_encode($data);
 }
 
 function getStoryPrompt($args) {
-	$data = [];
-	$data["type"] = "story";
-	$data["words"] = 1000;
-	$data["genre"] = "science fiction";
-	$data["keyword"] = "fish";
-	return json_encode($data);
+    $data = [];
+    $data["type"] = "story";
+    $data["words"] = 1000;
+    $data["genre"] = "science fiction";
+    $data["keyword"] = "fish";
+    return json_encode($data);
 }
 
 function getPrompt($args) {
-	$type = $args["type"];
-	if ($type == "poem") {
-		$prompt = getPoemPrompt($args);
-	}
-	else {
-		// Default to a story prompt:
-		$prompt = getStoryPrompt($args);
-	}
-	return $prompt;
+    if ($args["type"] == "poem")
+        return getPoemPrompt($args);
+
+    // Default to a story prompt:
+    return getStoryPrompt($args);
 }
 
 $prompt = getPrompt($_GET);
 echo $prompt;
-
-?>
